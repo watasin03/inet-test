b96379f84651ef82e744618aee0e77995be4662f
watasin03 <Wuttichai.Sr@cjexpress.co.th>
2021-06-10 13:16:41 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const app = require('express')()
+const cors = require('cors')
+const PORT = 3000
+
+app.use(cors())
+app.get('/test',(req,res)=>{
+    res.status(200).json({
+        message:"Hello"
+    })
+})
+
+app.listen(PORT,(err)=> err ? console.log(err) : console.log(`App Start @${PORT}`))
